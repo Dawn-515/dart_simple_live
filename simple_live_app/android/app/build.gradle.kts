8e7718b59ee437f998652e85ede4a40a1ddd218d
Xiao Chen <2497068245@qq.com>
2026-01-17 18:02:15 +0800

    修改包名为 com.dawn，版本号保持与原仓库一致 v1.11.1

--- simple_live_app/android/app/build.gradle.kts
+++ simple_live_app/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.xycz.simple_live"
+    namespace = "com.dawn"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.xycz.simple_live"
+        applicationId = "com.dawn"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ simple_live_app/android/app/src/main/kotlin/com/dawn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dawn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
